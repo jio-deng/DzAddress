55e7730443850190b40084fdcca442861160a4f0
dengzm <deng_zm@163.com>
2018-09-05 09:19:09 +0800

    0905 commit add: leakCanary检测内存泄漏(未监测到)

--- app/src/main/java/com/dz/address/base/MyApplication.java
+++ app/src/main/java/com/dz/address/base/MyApplication.java
@@ -3,6 +3,7 @@ package com.dz.address.base;
 import android.app.Application;
 
 import com.dz.address.utils.PreferenceUtil;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * @Description application
@@ -12,6 +13,12 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         PreferenceUtil.getInstance().init(this);
     }
 }
